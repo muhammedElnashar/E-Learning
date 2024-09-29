7be56de97ee913cfcdc16b629573f8ba89a6b3f9
abdelrahmanlghorab <abdoelghorab123@gmail.com>
2024-09-29 23:50:53 +0300

    add fuctionlty to scorecontroller

--- app/Http/Controllers/Api/ScoreController.php
+++ app/Http/Controllers/Api/ScoreController.php
@@ -1,11 +1,50 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Score;
 use Illuminate\Http\Request;
 
 class ScoreController extends Controller
 {
-    //
+    public function index()
+    {
+        return Score::all();
+    }
+
+    public function store(Request $request)
+    {
+        $score = Score::create($request->all());
+        return response()->json($score, 201);
+    }
+
+    public function show($id)
+    {
+        $score = Score::find($id);
+        if (!$score) {
+            return response()->json(['message' => 'Score not found'], 404);
+        }
+        return $score;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $score = Score::find($id);
+        if (!$score) {
+            return response()->json(['message' => 'Score not found'], 404);
+        }
+        $score->update($request->all());
+        return response()->json($score, 200);
+    }
+
+    public function destroy($id)
+    {
+        $score = Score::find($id);
+        if (!$score) {
+            return response()->json(['message' => 'Score not found'], 404);
+        }
+        $score->delete(); // Delete score
+        return response()->json(['message' => 'Score deleted'], 204);
+    }
 }
