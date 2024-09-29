9dcb9c7e7374dbc36e2e879f470e02104400ed75
Mohamed <mohamed.ashraf74@gmail.com>
2024-09-29 23:41:52 +0300

    update QuestionController

--- app/Http/Controllers/Api/QuestionController.php
+++ app/Http/Controllers/Api/QuestionController.php
@@ -1,11 +1,49 @@
 <?php
-
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function index()
+    {
+        return Question::all(); 
+    }
+
+    public function store(Request $request)
+    {
+        $question = Question::create($request->all()); 
+        return response()->json($question, 201); 
+    }
+
+    public function show($id)
+    {
+        $question = Question::find($id); 
+        if (!$question) {
+            return response()->json(['message' => 'Question not found'], 404); 
+        }
+        return $question; 
+    }
+
+    public function update(Request $request, $id)
+    {
+        $question = Question::find($id);
+        if (!$question) {
+            return response()->json(['message' => 'Question not found'], 404);
+        }
+        $question->update($request->all()); 
+        return response()->json($question, 200); 
+    }
+
+    public function destroy($id)
+    {
+        $question = Question::find($id);
+        if (!$question) {
+            return response()->json(['message' => 'Question not found'], 404);
+        }
+        $question->delete(); 
+        return response()->json(['message' => 'Question deleted'], 204); 
+    }
 }
