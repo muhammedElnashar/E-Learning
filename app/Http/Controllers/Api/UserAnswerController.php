2e7996a587510ee7c0b3c302d4d359d59e092ce7
abdelrahmanlghorab <abdoelghorab123@gmail.com>
2024-09-29 23:49:35 +0300

    add fuctionlty to useranswercontroller

--- app/Http/Controllers/Api/UserAnswerController.php
+++ app/Http/Controllers/Api/UserAnswerController.php
@@ -1,11 +1,51 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserAnswer;
 use Illuminate\Http\Request;
 
 class UserAnswerController extends Controller
 {
-    //
+    public function index()
+    {
+        return UserAnswer::all();
+    }
+
+    public function store(Request $request)
+    {
+        $userAnswer = UserAnswer::create($request->all());
+        return response()->json($userAnswer, 201);
+    }
+
+    public function show($id)
+    {
+        $userAnswer = UserAnswer::find($id);
+        if (!$userAnswer) {
+            return response()->json(['message' => 'User answer not found'], 404);
+        }
+        return $userAnswer;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $userAnswer = UserAnswer::find($id);
+        if (!$userAnswer) {
+            return response()->json(['message' => 'User answer not found'], 404);
+        }
+        $userAnswer->update($request->all());
+        return response()->json($userAnswer, 200);
+    }
+
+    public function destroy($id)
+    {
+        $userAnswer = UserAnswer::find($id);
+        if (!$userAnswer) {
+            return response()->json(['message' => 'User answer not found'], 404);
+        }
+        $userAnswer->delete();
+        return response()->json(['message' => 'User answer deleted'], 204);
+    }
 }
+
