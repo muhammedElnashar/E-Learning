a41a94c2e167b4a50c02c0ea12bb0420d04c53aa
MahmoudTtantawy <tantawym@gmail.com>
2024-10-06 16:29:43 +0300

    Create Crud for student and softDelete

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,9 @@ Route::post('/register', [RegisterationController::class,'Register']);
 Route::post('/login', [RegisterationController::class,'Login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/students', RegisterationController::class);
+    Route::post('/students/{id}/restore', [RegisterationController::class, 'restore']);
+    Route::get('/students/trashed', [RegisterationController::class, 'trashed']);
     Route::post('/logout', [RegisterationController::class,'Logout']);
     //organizer and teacher
     Route::get('/teacher', [organizarController::class, 'getAllTeachers']);
