330e2d5ef4861d902b8d69d7a32204f9e01d3b3d
omarabuelkhier <omarabuelkhier@gmail.com>
2024-10-13 20:57:21 +0300

    nothing

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -58,29 +58,29 @@ class PaymentController extends Controller
             DB::beginTransaction();
 
             $payment = Payment::create([
-            'user_id' => $user->id,
-            'course_id' => $request->course_id,
-            'amount' => $request->amount,
-            'payment_date' => now(),
-        ]);
-        $enrollment = Enrollment::create([
-            'user_id' => $user->id,
-            'course_id' => $request->course_id,
-        ]);
+                'user_id' => $user->id,
+                'course_id' => $request->course_id,
+                'amount' => $request->amount,
+                'payment_date' => now(),
+            ]);
+            $enrollment = Enrollment::create([
+                'user_id' => $user->id,
+                'course_id' => $request->course_id,
+            ]);
             $admin = User::where('role_id', 1)->first();
             $username = $user->name;
             $userImage = $user->image;
             $courseTitle = $course->title;
             $created_at= $payment->created_at;
-          Notification::send($admin,new AdminPayment($username,$userImage,$courseTitle,$created_at));
-          Notification::send($user,new UserPayment($username,$userImage,$courseTitle,$created_at));
+            Notification::send($admin,new AdminPayment($username,$userImage,$courseTitle,$created_at));
+            Notification::send($user,new UserPayment($username,$userImage,$courseTitle,$created_at));
 
-        DB::commit();
-        return response()->json(['message' => 'Payment successful and user enrolled.',
+            DB::commit();
+            return response()->json(['message' => 'Payment successful and user enrolled.',
             ], 200);
-    }else{
-        return response()->json(['error' => 'Payment failed.'], 400);
-    }
+        }else{
+            return response()->json(['error' => 'Payment failed.'], 400);
+        }
 
     }
     public function getPayments()
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,71 @@
 <?php
+//
+//use App\Http\Controllers\Api\CommentController;
+//use App\Http\Controllers\Api\RegisterationController;
+//use App\Http\Controllers\Api\PlaylistController;
+//use App\Http\Controllers\Api\organizarController;
+//use App\Http\Controllers\Api\TestController;
+//use App\Http\Controllers\Api\QuestionController;
+//use App\Http\Controllers\Api\AnswerController;
+//use App\Http\Controllers\Api\UserAnswerController;
+//use App\Http\Controllers\Api\ScoreController;
+//use Illuminate\Support\Facades\Route;
+//use App\Http\Controllers\Api\VideoController;
+//use App\Http\Controllers\Api\CourseController;
+//use App\Http\Controllers\Api\PaymentController;
+//use App\Http\Controllers\Api\EnrollmentController;
+//
+///*Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');*/
+//Route::post('/register', [RegisterationController::class,'Register']);
+//Route::post('/login', [RegisterationController::class,'Login']);
+//Route::post('/forget-password', [RegisterationController::class,'forgetPassword']);
+//
+//Route::middleware('auth:sanctum')->group(function () {
+//    Route::get('user-notifications',[RegisterationController::class,'getUserNotifications']);
+//    Route::post('read-notifications',[RegisterationController::class,'readUserNotifications']);
+//    Route::apiResource('/students', RegisterationController::class);
+//    Route::post('/student/{id}/restore', [RegisterationController::class, 'restore']);
+//    Route::get('/students-trashed', [RegisterationController::class, 'trashed']);
+//    Route::post('/logout', [RegisterationController::class,'Logout']);
+//    //organizer and teacher
+//    Route::get('/teacher', [organizarController::class, 'getAllTeachers']);
+//    Route::get('/teacher/{id}', [organizarController::class, 'getTeacher']);
+//    Route::apiResource('/organizar', organizarController::class);
+//    Route::post('/organizar/{id}/restore', [organizarController::class, 'restore']);
+//    Route::get('/organizartrashed', [organizarController::class, 'trashed']);
+//    //test
+//    Route::post("/upload-exam", [TestController::class,'storeExamFile']);
+//    Route::apiResource('tests', TestController::class);
+//    Route::get('tests-answer/{id}', [TestController::class,'ShowCorrectTestAnswer']);
+//    Route::apiResource('questions', QuestionController::class);
+//    Route::apiResource('answers', AnswerController::class);
+//    Route::apiResource('user-answers', UserAnswerController::class);
+//    Route::apiResource('scores', ScoreController::class);
+//    //Course
+//    Route::apiResource('/courses', CourseController::class);
+//    Route::apiResource('playlists', PlaylistController::class);
+//    Route::apiResource('videos', VideoController::class);
+//
+////payment
+//    Route::post('/create-payment-intent', [PaymentController::class, 'createPaymentIntent']);
+//    Route::post('/store-payment', [PaymentController::class, 'storePayment']);
+//    Route::get('/user-payments', [PaymentController::class, 'getPayments']);
+//    Route::get('/payments', [PaymentController::class, 'getAllPayments']);
+//    //enrollment
+//    Route::get('enrollments', [EnrollmentController::class, 'index']);
+//    Route::get('enrollments', [EnrollmentController::class, 'show']);
+//
+//
+////comment
+//Route::post('course/{course}', [CommentController::class, 'store']);
+//Route::get('course/{course}/comments', [CommentController::class, 'index']);
+//Route::delete('comment/{id}', [CommentController::class, 'destroy']);
+//Route::get('user-score/{id}',[RegisterationController::class, 'getScore']);
+//
+//});
+
 
 use App\Http\Controllers\Api\CommentController;
 use App\Http\Controllers\Api\RegisterationController;
@@ -18,33 +85,30 @@ use App\Http\Controllers\Api\EnrollmentController;
 /*Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');*/
-Route::post('/register', [RegisterationController::class,'Register']);
-Route::post('/login', [RegisterationController::class,'Login']);
-Route::post('/forget-password', [RegisterationController::class,'forgetPassword']);
+Route::post('/register', [RegisterationController::class, 'Register']);
+Route::post('/login', [RegisterationController::class, 'Login']);
+Route::post('/forget-password', [RegisterationController::class, 'forgetPassword']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('user-notifications',[RegisterationController::class,'getUserNotifications']);
-    Route::post('read-notifications',[RegisterationController::class,'readUserNotifications']);
+    Route::get('user-notifications', [RegisterationController::class, 'getUserNotifications']);
+    Route::post('read-notifications', [RegisterationController::class, 'readUserNotifications']);
     Route::apiResource('/students', RegisterationController::class);
     Route::post('/student/{id}/restore', [RegisterationController::class, 'restore']);
     Route::get('/students-trashed', [RegisterationController::class, 'trashed']);
-    Route::post('/logout', [RegisterationController::class,'Logout']);
+    Route::post('/logout', [RegisterationController::class, 'Logout']);
     //organizer and teacher
-    Route::get('/teacher', [organizarController::class, 'getAllTeachers']);
     Route::get('/teacher/{id}', [organizarController::class, 'getTeacher']);
     Route::apiResource('/organizar', organizarController::class);
     Route::post('/organizar/{id}/restore', [organizarController::class, 'restore']);
     Route::get('/organizartrashed', [organizarController::class, 'trashed']);
     //test
-    Route::post("/upload-exam", [TestController::class,'storeExamFile']);
-    Route::apiResource('tests', TestController::class);
-    Route::get('tests-answer/{id}', [TestController::class,'ShowCorrectTestAnswer']);
+    Route::post("/upload-exam", [TestController::class, 'storeExamFile']);
+    Route::get('tests-answer/{id}', [TestController::class, 'ShowCorrectTestAnswer']);
     Route::apiResource('questions', QuestionController::class);
     Route::apiResource('answers', AnswerController::class);
     Route::apiResource('user-answers', UserAnswerController::class);
     Route::apiResource('scores', ScoreController::class);
     //Course
-    Route::apiResource('/courses', CourseController::class);
     Route::apiResource('playlists', PlaylistController::class);
     Route::apiResource('videos', VideoController::class);
 
@@ -59,9 +123,11 @@ Route::middleware('auth:sanctum')->group(function () {
 
 
 //comment
-Route::post('course/{course}', [CommentController::class, 'store']);
-Route::get('course/{course}/comments', [CommentController::class, 'index']);
-Route::delete('comment/{id}', [CommentController::class, 'destroy']);
-Route::get('user-score/{id}',[RegisterationController::class, 'getScore']);
+    Route::post('course/{course}', [CommentController::class, 'store']);
+    Route::get('course/{course}/comments', [CommentController::class, 'index']);
+    Route::delete('comment/{id}', [CommentController::class, 'destroy']);
+    Route::get('user-score/{id}', [RegisterationController::class, 'getScore']);
 
 });
+Route::apiResource('/courses', CourseController::class);
+Route::get('/teacher', [organizarController::class, 'getAllTeachers']);
