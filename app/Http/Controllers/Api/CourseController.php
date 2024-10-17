9ca39832281634ad2c12ec92f2680139851485ae
abdelrahmanlghorab <abdoelghorab123@gmail.com>
2024-10-17 20:17:17 +0300

    adding the live courses feature

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -3,89 +3,85 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\UserResource;
 use App\Models\Course;
 use App\Models\Enrollment;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 
 class CourseController extends Controller
 {
-    /**
-     * Display a listing of the courses.
-     */
     public function index()
     {
         $courses = Course::all();
-        return $courses;
+        return response()->json($courses, 200);
     }
-
-    /**
-     * Store a newly created course in storage.
-     */
     public function store(Request $request)
     {
-        $courses=$request->all();
-
-        $validator = Validator::make($request->all(), [
+        $validationRules = [
             'title' => 'required|string|max:255',
             'description' => 'required',
             'price' => 'required_if:is_free,false|integer|min:0',
             'is_free' => 'required|boolean',
             'instructor_id' => 'required|exists:users,id',
-            'playlist_id' => 'required|exists:playlists,id',
             'course_type' => 'required|in:video,live',
-            'live_platform' => 'required_if:course_type,live|string|max:255',
-            'live_link' => 'required_if:course_type,live|url',
-            'live_schedule' => 'required_if:course_type,live|date',
-        ]);
+        ];
+
+        if ($request->course_type == 'live') {
+            $validationRules = array_merge($validationRules, [
+                'live_platform' => 'required|string|max:255',
+                'live_link' => 'required|url',
+                'live_schedule' => 'required|date',
+                'live_details' => 'required|string',
+            ]);
+        } else {
+            $validationRules = array_merge($validationRules, [
+                'playlist_id' => 'required|exists:playlists,id',
+            ]);
+        }
+
+        $validator = Validator::make($request->all(), $validationRules);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
-        $user=Auth::user();
-        // dd($user);
-        // if ($user->role_id != 4||$user->role_id != 1) {
-        //     return response()->json(['error' => 'You are not authorized to create courses.'], 403);
+        // $role_id =Auth::user()->role_id;
+        // if($role_id!=1||$role_id!=4||$role_id!=3){
+        //     return response()->json(['message' => 'Unauthorized'], 401);
         // }
-
         $course = Course::create([
             'title' => $request->title,
             'description' => $request->description,
             'price' => $request->is_free ? 0 : $request->price,
             'is_free' => $request->is_free,
             'instructor_id' => $request->instructor_id,
-            'playlist_id' => $request->playlist_id,
-            'thumbnail'=>$request->thumbnail,
+            'playlist_id' => $request->playlist_id ?? null, 
+            'thumbnail' => $request->thumbnail ?? 'https://campustechnology.com/-/media/EDU/CampusTechnology/2019-Images/20191209online.jpg',
             'course_type' => $request->course_type,
-            'live_platform' => $request->live_platform,
-            'live_link' => $request->live_link,
-            'live_schedule' => $request->live_schedule,
+            'live_platform' => $request->live_platform ?? null,
+            'live_link' => $request->live_link ?? null,
+            'live_schedule' => $request->live_schedule ?? null,
+            'live_details' => $request->live_details ?? null,
         ]);
 
         return response()->json($course, 201);
     }
 
-    /**
-     * Display the specified course.
-     */
+
     public function show($id)
     {
         $course = Course::find($id);
-        $enrollments = Enrollment::where('course_id', $id)->get();
-        $course->enrollments = $enrollments;
+
         if (!$course) {
             return response()->json(['message' => 'Course not found'], 404);
         }
 
+        $enrollments = Enrollment::where('course_id', $id)->get();
+        $course->enrollments = $enrollments;
+
         return response()->json($course, 200);
     }
 
-    /**
-     * Update the specified course in storage.
-     */
     public function update(Request $request, $id)
     {
         $course = Course::find($id);
@@ -94,15 +90,16 @@ class CourseController extends Controller
             return response()->json(['message' => 'Course not found'], 404);
         }
 
-
-        $validator = Validator::make($request->all(), [
+        $validationRules = [
             'title' => 'sometimes|required|string|max:255',
             'description' => 'sometimes|required',
             'price' => 'required_if:is_free,false|integer|min:0',
             'is_free' => 'sometimes|required|boolean',
             'instructor_id' => 'sometimes|required|exists:users,id',
             'playlist_id' => 'sometimes|required|exists:playlists,id',
-        ]);
+        ];
+
+        $validator = Validator::make($request->all(), $validationRules);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
@@ -113,9 +110,6 @@ class CourseController extends Controller
         return response()->json($course, 200);
     }
 
-    /**
-     * Remove the specified course from storage.
-     */
     public function destroy($id)
     {
         $course = Course::find($id);
@@ -125,6 +119,7 @@ class CourseController extends Controller
         }
 
         $course->delete();
+
         return response()->json(['message' => 'Course deleted successfully'], 200);
     }
 }
--- database/migrations/2024_10_14_152635_add_course_type_to_courses_table.php
+++ database/migrations/2024_10_14_152635_add_course_type_to_courses_table.php
@@ -6,26 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::table('courses', function (Blueprint $table) {
             $table->enum('course_type', ['video', 'live'])->default('video');
-            $table->string('live_platform')->nullable();
+            $table->string('live_platform')->nullable(); 
             $table->string('live_link')->nullable();
             $table->dateTime('live_schedule')->nullable();
+            $table->string('live_details')->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('courses', function (Blueprint $table) {
-            //
+            $table->dropColumn(['course_type', 'live_platform', 'live_link', 'live_schedule', 'live_details']);
         });
     }
 };
